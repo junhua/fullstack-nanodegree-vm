02441fbe5bbeda020afb02f79edd7a3bd5a18b18
Liu Junhua <junhua.viva@gmail.com>
2015-08-03 00:17:43 +0800

    passed all tests

--- vagrant/tournament/tournament.py
+++ vagrant/tournament/tournament.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# 
+#
 # tournament.py -- implementation of a Swiss-system tournament
 #
 
@@ -11,27 +11,74 @@ def connect():
     return psycopg2.connect("dbname=tournament")
 
 
+def execute(command):
+    conn = connect()
+    c = conn.cursor()
+
+    c.execute(command)
+
+    conn.commit()
+    conn.close()
+
+    return conn
+
+
+def execute_with_output(command):
+    conn = connect()
+    c = conn.cursor()
+
+    c.execute(command)
+
+    conn.commit()
+    result = c.fetchall()
+    conn.close()
+
+    return result
+
+
 def deleteMatches():
     """Remove all the match records from the database."""
+
+    execute("DELETE FROM Matches")
 
 
 def deletePlayers():
     """Remove all the player records from the database."""
+    execute("DELETE FROM Players")
 
 
 def countPlayers():
     """Returns the number of players currently registered."""
+    conn = connect()
+    c = conn.cursor()
+    c.execute("SELECT COUNT(*) from Players;")
+    conn.commit()
+
+    result = c.fetchone()[0]
+
+    conn.close()
+
+    return result
 
 
 def registerPlayer(name):
     """Adds a player to the tournament database.
-  
+
     The database assigns a unique serial id number for the player.  (This
     should be handled by your SQL database schema, not in your Python code.)
-  
+
     Args:
       name: the player's full name (need not be unique).
     """
+
+    # execute("INSERT INTO Players(uid, name) VALUES (DEFAULT,'%s');"%name)
+    conn = connect()
+    c = conn.cursor()
+
+    c.execute("INSERT INTO Players(uid, name) VALUES (DEFAULT,%s)", (name,))
+
+    conn.commit()
+    conn.close()
 
 
 def playerStandings():
@@ -48,6 +95,12 @@ def playerStandings():
         matches: the number of matches the player has played
     """
 
+    result = execute_with_output("""
+        select * from Standings
+        """)
+
+    return result
+
 
 def reportMatch(winner, loser):
     """Records the outcome of a single match between two players.
@@ -56,16 +109,18 @@ def reportMatch(winner, loser):
       winner:  the id number of the player who won
       loser:  the id number of the player who lost
     """
- 
- 
+    execute("INSERT INTO Matches (p1,p2,winner,loser) VALUES(%s,%s,%s,%s)" %
+            (winner, loser, winner, loser,))
+
+
 def swissPairings():
     """Returns a list of pairs of players for the next round of a match.
-  
+
     Assuming that there are an even number of players registered, each player
     appears exactly once in the pairings.  Each player is paired with another
     player with an equal or nearly-equal win record, that is, a player adjacent
     to him or her in the standings.
-  
+
     Returns:
       A list of tuples, each of which contains (id1, name1, id2, name2)
         id1: the first player's unique id
@@ -73,5 +128,15 @@ def swissPairings():
         id2: the second player's unique id
         name2: the second player's name
     """
+    players = [(row[0], row[1]) for row in playerStandings()]
 
+    assert len(players) >= 2, "Need more than 2 players to make a pair"
+    assert len(
+        players) % 2 == 0, "Need even number of players to make pairs, assuming no walk-over"
 
+    pairings = zip(players[0::2], players[1::2])
+
+    # flatten the pairings and convert back to a tuple
+    results = [tuple(list(sum(pairing, ()))) for pairing in pairings]
+
+    return results
--- vagrant/tournament/tournament.sql
+++ vagrant/tournament/tournament.sql
@@ -6,4 +6,44 @@
 -- You can write comments in this file by starting them with two dashes, like
 -- these lines here.
 
+CREATE DATABASE tournament;
+\c tournament;
+
+CREATE TABLE Players (
+	uid			SERIAL PRIMARY KEY,
+	name		VARCHAR(40)
+);
+
+
+CREATE TABLE Matches (
+	id			serial primary key,
+	p1			integer REFERENCES Players (uid),
+	p2			integer REFERENCES Players (uid),
+	winner		integer REFERENCES Players (uid),
+	loser		integer REFERENCES Players (uid)
+);
+
+-- Count number of wins
+
+CREATE VIEW WinCounts AS
+SELECT Players.uid, Players.name, COUNT(Matches.winner) AS wins 
+FROM Players LEFT JOIN Matches
+ON Players.uid = Matches.winner
+GROUP BY Players.uid;
+
+-- Total matches
+
+CREATE VIEW TotalMatches AS
+SELECT Players.uid, Players.name, COUNT(Matches.id) AS total_matches 
+FROM Players LEFT JOIN Matches
+ON Players.uid = Matches.p1 OR Players.uid = Matches.p2
+GROUP BY Players.uid;
+
+-- satndings
+
+CREATE VIEW Standings AS
+    SELECT WinCounts.uid, WinCounts.name, wins, total_matches 
+    FROM WinCounts LEFT JOIN TotalMatches
+    ON WinCounts.uid = TotalMatches.uid
+    ORDER BY wins desc;
 
